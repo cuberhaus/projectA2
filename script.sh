1734ff64fe11eca56e58e1744412e53c90daf65b
AleexP <95046637+AleexP@users.noreply.github.com>
2022-03-12 10:31:55 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+g++ -o v3 v3.cc
+
+i=1
+while [ $i -le 2 ]; do
+      if [ $i -eq 1 ]; then file="Binomial"
+      else file="Geometric"
+      fi
+      for filename1 in $file/*; do
+          name1=`basename -a $filename1`
+          for filename2 in $filename1/*; do 
+              name2=`basename -a $filename2`
+              mkdir -p "Out$file/$name1/$name2"
+              for filename3 in $filename2/*; do
+                  name3=`basename -a $filename3`        
+                  ./v3 <"$filename3" > "out$name3"
+                  mv "out$name3" "Out$file/$name1/$name2"
+                  done
+              done
+          done 
+      i=$(($i + 1))
+      done
+
+
