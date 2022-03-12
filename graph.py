5c199c0d5ab454c3e40fe92c25a157deacc0ccf0
Pol Casacuberta <polcg10@gmail.com>
2022-03-12 20:59:20 +0100

    percolated binomial graph

--- graph.py
+++ graph.py
@@ -8,6 +8,7 @@ import numpy as np
 
 directory_path = os.getcwd()
 
+
 def connected_plot(numbersx, numbersy, xlabel, nfigure, label, directory):
     label2 = str(label) + " Nodes"
     plt.plot(numbersx, numbersy, label=label2)
@@ -43,7 +44,7 @@ def binomial_graph():
         os.mkdir(directory_path + "/binomial_graph")
     f = open(directory_path + "/binomial_graph/binomial_graph_analysis.txt", "w")
 
-    times = 10
+    times = 10 # We try for every probability 10 times Ex: if two times the graph is connected then we have a 20% probability that it is indeed connected
     f.write("Sample size: " + str(times) + "\n")
     nplot = 0
     node_values = [5, 10, 20, 50, 100, 500, 1000]
@@ -73,6 +74,65 @@ def binomial_graph():
                        "/binomial_graph/plots/")
         nplot += 1
     f.close()
+
+def binomial_graph_percolation:
+    
+    print("Graph dirigit?: 0/1 ")
+    dirigit = int(input())
+
+    if not os.path.isdir(directory_path + "/binomial_graph"):
+        os.mkdir(directory_path + "/binomial_graph")
+    f = open(directory_path + "/binomial_graph/binomial_graph_analysis.txt", "w")
+
+    times = 10 # We try for every probability 10 times Ex: if two times the graph is connected then we have a 20% probability that it is indeed connected
+    f.write("Sample size: " + str(times) + "\n")
+    nplot = 0
+    node_values = [5, 10, 20, 50, 100]
+    # for Nnodes in np.linspace(20,1000,5):
+    for Nnodes in node_values:
+        numbers_x = []
+        numbers_y = []
+        numbers_xq = []
+        numbers_yq = []
+        chosen_p_q = 0
+        for prob in np.linspace(0, 1, 51):
+            n_connected = 0
+            for time in range(times):
+                bi_graph = nx.binomial_graph(Nnodes, prob, directed=dirigit)  # A.k.a. Erdos-Rényi graph
+                if nx.is_connected(bi_graph):
+                    n_connected = n_connected + 1
+                # Draw plots
+                # nx.draw(biGraph)
+                # plt.savefig(directory_path + "/binomial_graph/" + str(x) + ".png")
+                # plt.clf()
+                bi_graph.clear()
+            p_connected = n_connected / times
+            if p_connected == 1 and chosen_p_q != 1: chosen_p_q = prob
+            if p_connected < 1 and chosen_p_q == 1: chosen_p_q = prob
+            numbers_x.append(prob)
+            numbers_y.append(p_connected)
+            f.write("Nodes: " + str(Nnodes) + " Probability edge: " + str(prob) + " Connected probability: " + str(
+                p_connected) + "\n")
+        for probQ in np.linspace(0, 1, 51):
+            n_connected = 0
+            for time in range(times) :
+                bi_graph = nx.binomial_graph(Nnodes, chosen_p_q, directed=dirigit)
+                perc_bi_graph = node_percolation(bi_graph,probQ)
+                if nx.is_connected(perc_bi_graph):
+                    n_connected = n_connected + 1
+                bi_graph.clear()
+            p_connected = n_connected / times
+            numbers_xq.append(probQ)
+            numbers_yq.append(p_connected)
+            f.write("Nodes: " + str(Nnodes) + " Probability edge: " + str(probQ) + " Connected probability: " + str(
+                p_connected) + "\n")
+        print(numbers_x)
+        print(numbers_y)
+        connected_plot(numbers_x, numbers_y, "Probability that an edge is created", nplot, Nnodes,
+                       "/binomial_graph/plots/")
+        nplot += 1
+    f.close()
+    
 
 
 def random_geometric_graph():
@@ -111,36 +171,38 @@ def random_geometric_graph():
     f.close()
 
 
-def node_percolation(g):
-    print("p:")
-    p = float(input())
+def node_percolation(g,p):
+    # print("p:")
+    # p = float(input())
+    #
+    # print("Nodes: " + str(g.nodes()))
+    # print("Edges: " + str(g.edges()))
 
-    print("Nodes: " + str(g.nodes()))
-    print("Edges: " + str(g.edges()))
-
-    print("----Node Percolation-----")
+    # print("----Node Percolation-----")
     for i in range(g.number_of_nodes()):
         if random.random() > p:
             g.remove_node(i)
 
-    print("Nodes: " + str(g.nodes()))
-    print("Edges: " + str(g.edges()))
+    # print("Nodes: " + str(g.nodes()))
+    # print("Edges: " + str(g.edges()))
+    return g
 
 
-def edge_percolation(g):
-    print("p:")
-    p = float(input())
-
-    print("Nodes: " + str(g.nodes()))
-    print("Edges: " + str(g.edges()))
-
-    print("----Edge Percolation-----")
+def edge_percolation(g,p):
+    # print("p:")
+    # p = float(input())
+    #
+    # print("Nodes: " + str(g.nodes()))
+    # print("Edges: " + str(g.edges()))
+    #
+    # print("----Edge Percolation-----")
     for i in g.edges():
         if random.random() > p:
             g.remove_edge(*i)
 
-    print("Nodes: " + str(g.nodes()))
-    print("Edges: " + str(g.edges()))
+    # print("Nodes: " + str(g.nodes()))
+    # print("Edges: " + str(g.edges()))
+    return g
 
 
 def complex_connected_components(g):
@@ -162,19 +224,25 @@ def complex_connected_components(g):
 #                 g[i][j] = 0
 #     return g
 
-def graellaN(n):
+def graella_n(n):
     graella = nx.Graph()
-    for i in range(n*n):
+    for i in range(n * n):
         graella.add_node(i)
     # Arestes horitzontals
     for i in range(n):
-        for j in range(n-1):
-            graella.add_edge((i*n)+j, (i*n) + j+1)
+        for j in range(n - 1):
+            graella.add_edge((i * n) + j, (i * n) + j + 1)
     # Arestes verticals
-    for i in range(n-1):
+    for i in range(n - 1):
         for j in range(n):
-            graella.add_edge((i*n)+j, ((i+1)*n) + j)
+            graella.add_edge((i * n) + j, ((i + 1) * n) + j)
     return graella
+
+def percolate_graella():
+    print("Choose an N to generate an NxN grid")
+    for n_nodes
+    g = graella_n(n)
+
 
 selection = read_option()
 if selection == 1:
@@ -183,8 +251,10 @@ if selection == 1:
           "2- edge percolation\n")
     option = int(input())
     G = nx.binomial_graph(5, 0.8)
-    if option == 1: node_percolation(G)
-    else : edge_percolation(G)
+    if option == 1:
+        node_percolation(G)
+    else:
+        edge_percolation(G)
 elif selection == 2:
     binomial_graph()
 elif selection == 3:
@@ -192,10 +262,10 @@ elif selection == 3:
 elif selection == 4:
     print("Choose an N to generate an NxN grid")
     n = int(input())
-    g = graellaN(n)
+    g = graella_n(n)
     if not os.path.isdir(directory_path + "/graella"):
         os.mkdir(directory_path + "/graella")
-    nx.draw_networkx(g, with_labels = True)
+    nx.draw_networkx(g, with_labels=True)
     plt.savefig(directory_path + "/graella/" + "graella" + str(n) + ".png")
     plt.clf()
 
