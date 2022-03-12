e3201b3611d14657fe4cee4ed0c1d82d6d040680
Pol Casacuberta <polcg10@gmail.com>
2022-03-12 14:36:07 +0100

    fixed buggs

--- Connex i Complex.cc
+++ Connex i Complex.cc
@@ -4,109 +4,128 @@
 
 using namespace std;
 
-void leer_grafo(vector<vector<int> >& grafo, const int& n) {
-	
-	for (int i = 0; i < n; ++i) {
-	   int vecino;
-	   int node;
-	   cin >> node;
-	   while (cin >> vecino and vecino != -1) {
-		grafo[node].push_back(vecino);
-		grafo[vecino].push_back(node);
-	   }
-		
-	}
+void leer_grafo(vector<vector<int> >& grafo, const int& n)
+{
+
+    for (int i = 0; i < n; ++i)
+    {
+        int vecino;
+        int node;
+        cin >> node;
+        while (cin >> vecino and vecino != -1)
+        {
+            grafo[node].push_back(vecino);
+            grafo[vecino].push_back(node);
+        }
+
+    }
 
 }
 
 
-void escribir_grafo(const vector<vector<int> >& grafo) {
-	int n = grafo.size();
-	cout << "El nombre de nodes es: " << n << endl;
-	
-	for (int i = 0; i < n; ++i) {
-	   cout << i << ":";
-	   for (int j : grafo[i]) {
-	      cout << " " << j; 
-	   }
-	   cout << endl;
-	}
-	
+void escribir_grafo(const vector<vector<int> >& grafo)
+{
+    int n = grafo.size();
+    cout << "El nombre de nodes es: " << n << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << i << ":";
+        for (int j : grafo[i])
+        {
+            cout << " " << j;
+        }
+        cout << endl;
+    }
+
 }
 
 
-void cycles(const vector<vector<int> > &M, vector<bool> &visited, vector<int> &altura, const int &node, int &height, int &nr_cycles, int ant) {
-	altura[node] = height;
-	++height;
-	visited[node] = true;	
-	
-	for (int j = 0; j < M[node].size(); ++j) {   
+void cycles(const vector<vector<int> > &M, vector<bool> &visited, vector<int> &altura, const int &node, int &height, int &nr_cycles, int ant)
+{
+    altura[node] = height;
+    ++height;
+    visited[node] = true;
 
-	   int v = M[node][j];
-	   if (v != ant) { 
-	     if (visited[v] == true and altura[v] < altura[node]) {
-	       ++nr_cycles;
-	     }
-	     if (visited[v] == false) cycles(M, visited, altura, v, height, nr_cycles, node);
-	   }
-	}
+    for (int j = 0; j < M[node].size(); ++j)
+    {
+
+        int v = M[node][j];
+        if (v != ant)
+        {
+            if (visited[v] == true and altura[v] < altura[node])
+            {
+                ++nr_cycles;
+            }
+            if (visited[v] == false) cycles(M, visited, altura, v, height, nr_cycles, node);
+        }
+    }
 }
 
-void dfs(const vector<vector<int> > &M, vector<bool> &visitats, const int &node) {
-	stack<int> S;
-	S.push(node);
-	visitats[node] = true;
-	int m = M[0].size();
-	
-	while (not S.empty()) {
-	     int currentNode = S.top();
-	     S.pop();
-	     for (int j = 0; j < M[currentNode].size(); ++j) {
-		int l = M[currentNode][j];
-		if (not visitats[l] and l) {
-		  S.push(l);
-		  visitats[l] = true;
-		}
-	     }
-	}
+void dfs(const vector<vector<int> > &M, vector<bool> &visitats, const int &node)
+{
+    stack<int> S;
+    S.push(node);
+    visitats[node] = true;
+    int m = M[0].size();
+
+    while (not S.empty())
+    {
+        int currentNode = S.top();
+        S.pop();
+        for (int j = 0; j < M[currentNode].size(); ++j)
+        {
+            int l = M[currentNode][j];
+            if (not visitats[l] and l)
+            {
+                S.push(l);
+                visitats[l] = true;
+            }
+        }
+    }
 }
 
-pair<int, bool> countCC(const vector<vector<int> > &M) {
-	int cc = 0;
-	int n = M.size();
-	vector<bool> visitats(n, false);
-	bool complexes = true;
-	
-	for (int i = 0; i < n; ++i) {
-	   if (not visitats[i]) {
-	     dfs(M, visitats, i);
-	     ++cc;	
-	     if (complexes) {
-	       vector<int> altura(n, 1); 
-	       vector<bool> visited (n, false);
-	       int a = 1;
-	       int nr_c = 0;
-	       cycles(M, visited, altura, i, a, nr_c, -1);
-	       cout << "nr_c : " << nr_c << endl;
-		   
-	       if (nr_c <= 1) complexes = false;
-			
-	       }
-	   }
-	}
-	return make_pair(cc, complexes);
+pair<int, bool> countCC(const vector<vector<int> > &M)
+{
+    int cc = 0;
+    int n = M.size();
+    vector<bool> visitats(n, false);
+    bool complexes = true;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (not visitats[i])
+        {
+            dfs(M, visitats, i);
+            ++cc;
+            if (complexes)
+            {
+                vector<int> altura(n, 1);
+                vector<bool> visited (n, false);
+                int a = 1;
+                int nr_c = 0;
+                cycles(M, visited, altura, i, a, nr_c, -1);
+                cout << "nr_c : " << nr_c << endl;
+
+                if (nr_c <= 1) complexes = false;
+
+            }
+        }
+    }
+    return make_pair(cc, complexes);
 }
 
-int main() {
-	int n;
-	cin >> n;
-	
-	vector<vector<int> > graph(n);
-	leer_grafo(graph, n);
-	escribir_grafo(graph);
-	pair <int, bool> p  = countCC(graph);
-	if (p.second == true) cout << "El nombre de components connexes del graf es : " << p.first << " El graf es complex" << endl;
-	else cout << "Nombre de components connexes del graf: " << p.first << " El graf no es complex" << endl;
+int main()
+{
+    int n;
+    cin >> n;
 
-	
+    vector<vector<int> > graph(n);
+    leer_grafo(graph, n);
+    escribir_grafo(graph);
+    pair <int, bool> p  = countCC(graph);
+    if (p.second == true) cout << "El nombre de components connexes del graf es : " << p.first << " El graf es complex" << endl;
+    else cout << "Nombre de components connexes del graf: " << p.first << " El graf no es complex" << endl;
+
+
 }
--- Generador.cc
+++ Generador.cc
@@ -1,3 +1,4 @@
+// WARNING: compile with -std=c++17
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,134 +6,155 @@
 #include <math.h>
 #include <unistd.h>
 #include <filesystem>
+#include <sstream>
 
 using namespace std;
 
-struct Point {
+struct Point
+{
     double x;
     double y;
 };
 
 
-void escribir_grafo(const vector<vector<int>>& grafo, ofstream& file) {
-	int n = grafo.size();
-	file << n << endl;
-	for (int i = 0; i < n; ++i) {
-	   file << to_string(i);
-	   for (int j : grafo[i]) {
-	      file << " " << j; 
-	   }
-	   file << " -1" << endl;
-	}
+void escribir_grafo(const vector<vector<int> >& grafo, ofstream& file)
+{
+    int n = grafo.size();
+    file << n << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        file << to_string(i);
+        for (int j : grafo[i])
+        {
+            file << " " << j;
+        }
+        file << " -1" << endl;
+    }
 }
 
-const void printPointVector(const vector<Point> &V) {
-	int n = V.size();
-	for (int i = 0; i < n; i++) cout << "(" << V[i].x << "," << V[i].y << ")" << " ";
-	cout << endl;
+const void printPointVector(const vector<Point> &V)
+{
+    int n = V.size();
+    for (int i = 0; i < n; i++) cout << "(" << V[i].x << "," << V[i].y << ")" << " ";
+    cout << endl;
 }
 
-float generateNumber() {
-	return (float(rand()) / float(RAND_MAX));
+float generateNumber()
+{
+    return (float(rand()) / float(RAND_MAX));
 }
 
-Point readPoint() {
+Point readPoint()
+{
     Point a;
     cin >> a.x >> a.y;
     return a;
 }
 
-double distance(Point a, Point b) {
-    return sqrt(pow(b.x-a.x, 2)+pow(b.y-a.y, 2));
+double distance(Point a, Point b)
+{
+    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
 }
 
-vector< vector<int> > binomialRandomGraph(int n, float p) { 
+vector< vector<int> > binomialRandomGraph(int n, float p)
+{
     vector< vector<int> > G(n);
-    for (int i = 0; i<n; ++i) {
-       for (int j = i; j<n; ++j) {
-	  if (j != i and generateNumber()<=p) G[i].push_back(j);
-       }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i; j < n; ++j)
+        {
+            if (j != i and generateNumber() <= p) G[i].push_back(j);
+        }
     }
-    
+
     return G;
 }
 
-vector< vector<int> > randomGeometricGraph(int n, float r) { 
+vector< vector<int> > randomGeometricGraph(int n, float r)
+{
     vector<Point> V(n);
-    for (int i = 0; i<n; ++i) {
-       V[i].x = generateNumber();
-       V[i].y = generateNumber();
+    for (int i = 0; i < n; ++i)
+    {
+        V[i].x = generateNumber();
+        V[i].y = generateNumber();
     }
     vector< vector<int> > G(n);
-    for (int i = 0; i<n; ++i){
-       for (int j = i; j<n; ++j){
-	  if (i != j and distance(V[i], V[j])<=r) {
-	    G[i].push_back(j); 
-	  }
-       }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i; j < n; ++j)
+        {
+            if (i != j and distance(V[i], V[j]) <= r)
+            {
+                G[i].push_back(j);
+            }
+        }
     }
-    
+
 //     printPointVector(V);
     return G;
 }
 
 
-void generate_graphs(const int& iterations, string& type_graph) {
-	
-	filesystem::create_directories(type_graph);
-	chdir(&type_graph[0]);
-	
-	for (int n = 20; n <= iterations; n += 20) {
-	   string it1 = to_string(n);
-	   for (int i = 1; i <= 100; ++i) {
-	      for (int j = 0; j < 20; ++j) {  
-		 ofstream file;
-		 float p_r = i/100.0;
-			 
-		 stringstream s;
-		 s << p_r;
-		 string it2 = s.str();
-		 string it3 = to_string(j);
-			 
-		 //Creates directory of the numbers of nodes 
-		 filesystem::create_directories(it1);
-	         chdir(&it1[0]);
-			 
-		 //Creates directory of the numbers of p_r
-		 filesystem::create_directories(it2);
-	         chdir(&it2[0]);
-			 
-		 file.open(type_graph+" iteracions: "+it3);
-		 // return to directory n
-		 chdir("..");
-			 
-		 vector< vector<int> > G;
-		 if (type_graph == "Binomial") G = binomialRandomGraph(n, p_r); 
-		 else G = randomGeometricGraph(n, p_r);
-		 escribir_grafo(G, file);
-		 chdir("..");
-	      }
-	  }
-	}
-	chdir("..");
+void generate_graphs(const int& iterations, string& type_graph)
+{
+
+    filesystem::create_directories(type_graph);
+    chdir(&type_graph[0]);
+
+    for (int n = 20; n <= iterations; n += 20)
+    {
+        string it1 = to_string(n);
+        for (int i = 1; i <= 100; ++i)
+        {
+            for (int j = 0; j < 20; ++j)
+            {
+                ofstream file;
+                float p_r = i / 100.0;
+
+                stringstream s;
+                s << p_r;
+                string it2 = s.str();
+                string it3 = to_string(j);
+
+                //Creates directory of the numbers of nodes
+                filesystem::create_directories(it1);
+                chdir(&it1[0]);
+
+                //Creates directory of the numbers of p_r
+                filesystem::create_directories(it2);
+                chdir(&it2[0]);
+
+                file.open(type_graph + " iteracions: " + it3);
+                // return to directory n
+                chdir("..");
+
+                vector< vector<int> > G;
+                if (type_graph == "Binomial") G = binomialRandomGraph(n, p_r);
+                else G = randomGeometricGraph(n, p_r);
+                escribir_grafo(G, file);
+                chdir("..");
+            }
+        }
+    }
+    chdir("..");
 }
 
 int main()
 {
     srand((int)time(0));
-	int iterations;
-	
-	cout << "Introdueix el nombre de iteracions per generar el graf Binomial" << endl; 
-	cout << "(cada iteracio va variant de 20 en 20): " << endl;
-	cin >> iterations; 
-    
-	string type_graph = "Binomial";
-	generate_graphs(iterations, type_graph);
-	
-	cout << "Introdueix el nombre de iteracions per generar el graf Geometric" << endl;
-	cout << "(cada iteracio va variant de 20 en 20): " << endl;
-	cin >> iterations; 
-		
-	type_graph = "Geometric";
-	generate_graphs(iterations, type_graph);
+    int iterations;
+
+    cout << "Introdueix el nombre de iteracions per generar el graf Binomial" << endl;
+    cout << "(cada iteracio va variant de 20 en 20): " << endl;
+    cin >> iterations;
+
+    string type_graph = "Binomial";
+    generate_graphs(iterations, type_graph);
+
+    cout << "Introdueix el nombre de iteracions per generar el graf Geometric" << endl;
+    cout << "(cada iteracio va variant de 20 en 20): " << endl;
+    cin >> iterations;
+
+    type_graph = "Geometric";
+    generate_graphs(iterations, type_graph);
 }
