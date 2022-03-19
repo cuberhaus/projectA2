dc9e9db46bd49284914546f1621252c51c9dd923
Pol Casacuberta <polcg10@gmail.com>
2022-03-19 16:10:40 +0100

    This is a placeholder

--- graph3.py
+++ graph3.py
@@ -192,8 +192,8 @@ def binomial_graph_percolation(percolation_func, x_label, directory):
     # node_values = [5, 10, 20, 50, 100, 500, 1000]
     # p_gen_connected_graph = [0.8, 0.5,0.3, 0.15, 0.1, 0.05, 0.05]
     # node_values = [10, 20, 50, 100, 500, 1000, 2500]
-    node_values = [10, 20, 50, 100, 500]
-    p_gen_connected_graph = [0.5, 0.3, 0.14, 0.1, 0.1]  # les probabilitats han de coincidir amb el nom d'un arxiu
+    node_values = [10, 20, 50, 100, 500, 1000, 2000, 5000, 10000]
+    p_gen_connected_graph = [0.5, 0.30000000000000004, 0.2, 0.1, 0.1, 0.1,0.1,0.1]  # les probabilitats han de coincidir amb el nom d'un arxiu
     # p_gen_connected_graph = [0.5, 0.3, 0.15, 0.1, 0.1, 0.1, 0.1]
     p_gen = 0
     for Nnodes in tqdm(node_values, desc="Nodes"):
