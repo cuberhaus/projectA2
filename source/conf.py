8cb08dc990c1f84b629b4ad5b9f78728043c636c
Pol Casacuberta <polcg10@gmail.com>
2022-03-20 15:38:45 +0100

    changed sphinx theme successfully, had to import shpinx theme in conf.py

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -32,6 +33,7 @@ release = '1.0'
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,8 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-# html_theme = 'sphynx_rtd_theme'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+# html_theme_path = ["/Users/pol/Library/Python/3.8/lib/python/site-packages/sphinx_rtd_theme/"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
